8fe4511252a013d1fb8570ab716797ac7b27d72a
Viktor Koop <viktor.koop@gmail.com>
2013-07-04 23:49:20 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+mib-vm
+========
+1. Install [Vagrant](http://www.vagrantup.com/)
+2. Install [VirtualBox](https://www.virtualbox.org/wiki/Downloads) 
+3. git clone --recursive https://github.com/vkoop/mib-vm.git
+4. cd mib-vm
+5. vagrant up
+6. Polli should be reachable on http://localhost:8081
---
+++ puppet/manifests/base.pp
@@ -0,0 +1,68 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+# setup apt module
+class { 'apt':
+  always_apt_update    => false,
+  disable_keys         => undef,
+  proxy_host           => false,
+  proxy_port           => '8080',
+  purge_sources_list   => false,
+  purge_sources_list_d => false,
+  purge_preferences_d  => false
+}
+
+exec { "apt-get-update":
+  command => "apt-get update",
+  provider => shell,
+}
+
+package { 'git':
+        ensure => installed,
+}
+
+vcsrepo { "/usr/src/mibserver":
+  provider => git,
+  source => 'https://github.com/vkoop/mib-server.git',
+  revision => 'master',
+  ensure => latest,
+  require => Package['git'],
+}
+
+
+$qrdeps = [ "build-essential", "pkg-config", "libcairo2-dev", "imagemagick", "nodejs"]
+
+apt::ppa { "ppa:chris-lea/node.js": }
+->
+package { $qrdeps: ensure => "installed",
+  require => Exec['apt-get-update']
+}
+->
+exec { "install npm deps": 
+  command => "cd /usr/src/mibserver; npm install;",
+  provider => shell,
+  require => Vcsrepo['/usr/src/mibserver'],
+}
+
+file{"/etc/init/mibserver.conf":
+  ensure => "present",
+  owner => "root",
+  group => "root",
+  mode  => 770,
+  source => "/tmp/vagrant-puppet/templates/mibserver.upstart.conf",
+}
+
+
+
+exec { "install coffeescript":
+  command => "npm -g install coffee-script;",
+  provider => shell,
+  require => Exec['install npm deps']
+}
+->
+service { 'polli':
+  ensure => running,
+  enable => true,
+  subscribe => [
+    File["/etc/init/mibserver.conf"],
+  ]
+}
